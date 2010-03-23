4a5500e50401ac153e7d5343433003ae142fae8c
Patrick Borgeat <patrick@borgeat.de>
2010-03-24 00:39:39 +0100

    licensing

--- minifont.h
+++ minifont.h
@@ -1,10 +1,22 @@
 /*
- *  minifont.h
- *  waveformgen
- *
- *  Created by Patrick Borgeat on 23.03.10.
- *  Copyright 2010 cappel-nord. All rights reserved.
- *
+ minifont.h
+ Created by Patrick Borgeat on 23.03.2010
+ http://www.cappel-nord.de
+ 
+ This file is part of waveformgen.
+ 
+ waveformgen is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ waveformgen is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with waveformgen. If not, see <http://www.gnu.org/licenses/>.
  */
 
 const char wfg_char_table[] = {
